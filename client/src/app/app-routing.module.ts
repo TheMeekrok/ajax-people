81c5570f73529370c953ed5322b0688d8ad1989a
Artyom Kurpas <minicoal@gmail.com>
2023-05-28 18:59:17 +1000

    Add linter, fix routing, code refactor

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
-import {MainLayoutComponent} from "./shared/layouts/main-layout/main-layout.component";
+import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
+import { MainLayoutComponent } from "./shared/layouts/main-layout/main-layout.component";
 
 const routes: Routes = [
   {
